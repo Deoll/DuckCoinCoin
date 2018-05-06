ab2f2f727f5bd91af79b1b790e38b9bf7d8fa855
NoSylhex <36363833+NoSylhex@users.noreply.github.com>
2018-05-06 09:46:53 +0200

    Update cheater.h

--- DuckCoinCoin-C/cheater.h
+++ DuckCoinCoin-C/cheater.h
@@ -4,11 +4,20 @@
 
 #include "blockchain.h"
 
-
+/**
+ * @brief delete block at indexBlock and rebuild the blockchain
+ * 
+ * @param blockchain the Blockchain to modify
+ * @param indexBlock the index of the block to delete
+ */
 void cheater_block(Blockchain blockchain, int indexBlock);
-
-
-
+/**
+ * @brief delete transaction at indexTransaction from a block and rebuild Blockchain
+ * 
+ * @param blockchain the Blockchain to modify
+ * @param indexBlock the index of the block to delete the transaction
+ * @param indexTransaction the index of the transaction to delete
+ */
 void cheater_transaction(Blockchain blockchain, int indexBlock, int indexTransaction);
 
 
